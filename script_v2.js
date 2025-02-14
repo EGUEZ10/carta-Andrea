adb9e31cccfa443f31e031bb3e37d56a7d1ebe20
Tu Nombre <creiticalopseguez10@gmail.com>
2025-02-14 17:25:45 -0400

    first commit

---
+++ script_v2.js
@@ -0,0 +1,39 @@
+const envoltura = document.querySelector(".envoltura-sobre");
+const carta = document.querySelector(".carta");
+
+const audio = new Audio("andreafavorita.mp4"); 
+
+document.addEventListener("click", (e) => {
+
+    if (e.target.matches(".sobre") || 
+        e.target.matches(".solapa-derecha") ||
+        e.target.matches(".solapa-izquierda") ||
+        e.target.matches(".corazon")) {
+        envoltura.classList.toggle("abierto");
+
+        if (audio.paused) {
+            audio.play().catch(error => console.log("Error al reproducir el audio:", error));
+        }
+
+      
+    } else if (e.target.matches(".sobre *")) {
+        if (!carta.classList.contains("abierta")) {
+            carta.classList.add("mostrar-carta");
+
+            setTimeout(() => {
+                carta.classList.remove("mostrar-carta");
+                carta.classList.add("abierta");
+            }, 500);
+            envoltura.classList.add("desactivar-sobre")
+        } else {
+            carta.classList.add("cerrando-carta");
+            envoltura.classList.remove("desactivar-sobre");
+
+            setTimeout(() => {
+                carta.classList.remove("cerrando-carta")
+                carta.classList.remove("abierta")
+            }, 500);
+        }
+
+    } 
+})
